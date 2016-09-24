5382d355a638e6a977c9cfc1233f04c25ef76bd7
Janet jeffus <jjeffus@gmail.com>
2016-09-23 18:14:22 -0700

    updating to add congratulations

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,9 +1,13 @@
 class PeopleController < InheritedResources::Base
 
+  def show
+    @person = Person.find(params[:id])
+  end
+
   private
 
     def person_params
-      params.require(:person).permit(:real_gender, :dead_name, :rail_name, :address, :city, :state, :zip, :court_address, :court_location, :court_name, :dob, :born, :telephone, :delivered_at, :email)
+      params.require(:person).permit(:real_gender, :dead_name, :real_name, :address, :city, :state, :zip, :court_address, :court_location, :court_name, :dob, :born, :telephone, :delivered_at, :email)
     end
 end
 
